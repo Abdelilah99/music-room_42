b77d0aeba35448dc91b64a2de37feb98253e9764
Akram Guediri <akramguediri2@gmail.com>
2026-06-04 17:23:44 +0200

    feat: reemplementation of the kotlin methodchannel

--- android/app/src/main/kotlin/com/musicroom/music_room/MainActivity.kt
+++ android/app/src/main/kotlin/com/musicroom/music_room/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.musicroom.music_room
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val channel = "com.musicroom/device_info"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channel,
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getModel"        -> result.success(Build.MODEL)
+                "getManufacturer" -> result.success(Build.MANUFACTURER)
+                "getDeviceName"   -> result.success("${Build.MANUFACTURER} ${Build.MODEL}")
+                else              -> result.notImplemented()
+            }
+        }
+    }
+}
