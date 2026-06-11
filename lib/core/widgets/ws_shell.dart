09d1769aa8b659235393d23bbd404b39c98cc71a
Akram Guediri <akramguediri2@gmail.com>
2026-06-11 17:27:48 +0200

    minor fixes

--- lib/core/widgets/ws_shell.dart
+++ lib/core/widgets/ws_shell.dart
@@ -11,14 +11,12 @@ class WsShell extends StatelessWidget {
     required this.state,
     required this.onRetry,
     required this.child,
-    this.floatingActionButton,
   });
 
   final String title;
   final WsConnectionState state;
   final VoidCallback onRetry;
   final Widget child;
-  final Widget? floatingActionButton;
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +34,6 @@ class WsShell extends StatelessWidget {
         WsConnectionState.error => _ErrorView(onRetry: onRetry),
         _ => child,
       },
-      floatingActionButton:
-          state == WsConnectionState.error ? null : floatingActionButton,
     );
   }
 }
